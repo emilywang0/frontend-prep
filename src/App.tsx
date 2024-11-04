47e7d93cc08a094622504f269a1f612a72f3354d
Emily Wang <emilywang@dhcp-206-12-136-102.ubcsecure.wireless.ubc.ca>
2024-11-03 23:03:57 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        <span>cool</span> Hello World
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
