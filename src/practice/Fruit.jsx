03b9b16927dbbb6be3be4e51a6d04132e362aef4
Emily Wang <emilywang0416@gmail.com>
2024-12-01 23:14:13 -0800

    practice useState react hook

---
+++ src/practice/Fruit.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+export default function Fruit() {
+  const fruits = [
+    "Apple",
+    "Apricot",
+    "Banana",
+    "Blueberry",
+    "Cherry",
+    "Cranberry",
+    "Date",
+    "Dragonfruit",
+    "Elderberry",
+    "Fig",
+    "Grape",
+    "Grapefruit",
+  ];
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [caseSensitive, setCaseSensitive] = useState(false);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleCaseSensitive = (e) => {
+    setCaseSensitive(!caseSensitive);
+  };
+
+  const filteredFruits = fruits.filter((fruit) => {
+    if (caseSensitive) {
+      return fruit.includes(searchTerm);
+    } else {
+      return fruit.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
+    }
+  });
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        id="case-sensitive"
+        onChange={handleCaseSensitive}
+      />
+      <label for="case-sensitive">Case Sensitive? </label>
+      <input type="text" placeholder="Search here..." onChange={handleSearch} />
+      ;
+      {filteredFruits.length == 0 ? (
+        <p> No fruits found! </p>
+      ) : (
+        filteredFruits.map((fruit) => {
+          return <p> {fruit} </p>;
+        })
+      )}
+    </div>
+  );
+}
